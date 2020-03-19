3c980dc1f674589323e81986a30458816c854829
prihuda <rqul22@gmail.com>
2020-03-19 12:33:09 +0700

    ganti ke posgresql demi heroku

--- config/config.js
+++ config/config.js
@@ -1,16 +1,15 @@
 require('dotenv').config();//instatiate environment variables
-
 let CONFIG = {} //Make this global to use all over the application
 
 CONFIG.app          = process.env.APP   || 'dev';
 CONFIG.port         = process.env.PORT  || '3000';
 
-CONFIG.db_dialect   = process.env.DB_DIALECT    || 'mysql';
-CONFIG.db_host      = process.env.DB_HOST       || 'localhost';
-CONFIG.db_port      = process.env.DB_PORT       || '3306';
-CONFIG.db_name      = process.env.DB_NAME       || 'short_url';
-CONFIG.db_user      = process.env.DB_USER       || 'root';
-CONFIG.db_password  = process.env.DB_PASSWORD   || '';
+CONFIG.db_dialect   = process.env.DB_DIALECT
+CONFIG.db_host      = process.env.DB_HOST
+CONFIG.db_port      = process.env.DB_PORT
+CONFIG.db_name      = process.env.DB_NAME
+CONFIG.db_user      = process.env.DB_USER
+CONFIG.db_password  = process.env.DB_PASSWORD
 
 CONFIG.jwt_encryption  = process.env.JWT_ENCRYPTION || 'jwt_please_change';
 CONFIG.jwt_expiration  = process.env.JWT_EXPIRATION || '10000';
